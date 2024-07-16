0bbe93c4b945245144b064b563466a43b904872a
hasanalam24 <hasanalam365@gmail.com>
2024-07-16 14:21:14 +0600

    post method user done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
@@ -7,6 +8,12 @@ require('dotenv').config()
 
 app.use(express.json())
 app.use(cors())
+
+
+
+
+// console.log(hashPass)
+// const salt = bcrypt.genSal
 
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.qvnsypp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,6 +27,11 @@ const client = new MongoClient(uri, {
     }
 });
 
+async function hashPass(password) {
+    const res = await bcrypt.hash(password, 10)
+    return res
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -31,7 +43,36 @@ async function run() {
 
         app.post('/users', async (req, res) => {
             const userInfo = req.body;
-            console.log(userInfo)
+
+            const data = {
+                name: userInfo.name,
+                password: userInfo.password,
+                phone: userInfo.phone,
+                email: userInfo.email,
+                role: userInfo.role,
+                action: userInfo.action,
+                status: userInfo.status
+            }
+
+            console.log('data:', data)
+
+
+            const checking = await usersCollection.findOne({ phone: userInfo.phone })
+
+            console.log('email:', checking)
+            try {
+                if (checking && checking.phone === userInfo.phone) {
+                    res.send('User already exists')
+                } else {
+                    const result = await usersCollection.insertOne(data)
+                    res.send(result)
+                }
+
+            }
+            catch {
+                res.send('wrong Input')
+            }
+
         })
 
         // Send a ping to confirm a successful connection
